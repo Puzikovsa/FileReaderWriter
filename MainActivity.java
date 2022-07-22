0103369f1915b6d75dae4b8e43c8ea49054dce76
Sergey <s.puzikov59@gmail.com>
2022-07-22 12:32:35 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,87 @@
+package ru.synergy.readerwriter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String FILE_NAME = "content.txt";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private File getExternalFilePath(){
+        return new File(getExternalFilesDir(null), FILE_NAME);
+    }
+
+    public void onRead(View view){
+        FileOutputStream fos = null;
+
+        try {
+            EditText textBox = findViewById(R.id.reader);
+            String text = textBox.getText().toString();
+
+            fos = new FileOutputStream(getExternalFilePath());
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Файл успешно сохранен", Toast.LENGTH_LONG).show();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }finally {
+            try {
+                if(fos != null){
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void openText(View view){
+        FileInputStream fin = null;
+        TextView textView = findViewById(R.id.tv_write);
+        File file = getExternalFilePath();
+
+        try {
+            fin = new FileInputStream(file);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            textView.setText(text);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }finally {
+            try {
+                if(fin != null){
+                    fin.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
